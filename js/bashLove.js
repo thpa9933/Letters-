0da65d67a2773896ef77b7cf43e65fa10fff6435
Thomas Payne <thpa9933@colorado.edu>
2018-01-26 10:33:20 -0700

    JS pt.1

--- js/bashLove.js
+++ js/bashLove.js
@@ -1,4 +1,11 @@
-function expand(){
-  console.log("it works");
-  $("expand").toggle().show();
+
+function main(){
+  $('.wrapper').hide();
+  $('.wrapper').fadeIn(1000);
+  $('.A1Content').hide();
+  $('.A1').on('click',function(){
+    $('.A1Content').slideToggle(500);
+  })
 }
+
+$(document).ready(main); //runs the main function when the document has loaded
