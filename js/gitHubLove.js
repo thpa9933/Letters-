8d4e0afb2874ce505c293294c4050f8e5d4a71ae
Thomas Payne <thpa9933@colorado.edu>
2018-02-01 14:01:50 -0700

    working on JS

--- js/gitHubLove.js
+++ js/gitHubLove.js
@@ -1,8 +1,22 @@
 
-function main(){
-  
+
+// function main(){
+// $('.circle1').toggleClass('active');
+// }
+$('.insidecircle1').hide();
+
+setTimeout(function(){
+$('.circle1').toggleClass('active');
+$('.circle2').toggleClass('active');
+$('.circle3').toggleClass('active');
+$('.circle4').toggleClass('active');
+}, 3600);
+
+
+function change(x){
+  if(x == 1){
+    $('.insidecircle1').show().slideDown(500);
+  }
 }
 
-
-
-$(document).ready(main);
+//$(document).ready(main); //runs the main function when the document has loaded
