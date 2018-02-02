54564fed934b930232240f78d05343f3c212c189
Thomas Payne <thpa9933@colorado.edu>
2018-02-02 11:46:15 -0700

    fix BASH bug, smoother trans

--- js/gitLove.js
+++ js/gitLove.js
@@ -15,9 +15,11 @@
 //
 // $(document).ready(main); //runs the main function when the document has loaded
 
+  $('.bodyP').hide();
 function activate(){
   $('.leftHeart').toggleClass('leftMove');
   $('.leftDagger').toggleClass('leftDaggerActive');
+  $('.bodyP').show();
   $('.bodyP').toggleClass('bodyPActive');
   $('.rightHeart').toggleClass('rightMove');
   $('.rightDagger').toggleClass('rightDaggerActive');
